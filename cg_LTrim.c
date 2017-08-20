8dbb6aafd8d4f47612cfd3c6e1f841d5ff1e7d90
Amit Singh <amitsingh007s@gmail.com>
2017-08-20 14:51:02 +0530

    Update cg_LTrim.c

--- cg_LTrim.c
+++ cg_LTrim.c
@@ -3,8 +3,6 @@
 /* Purpose      : Trims spaces on the left of the string
 /* Input        : source string and the string to be trimmed
 /* Output       : left trimmed string
-/* Created by   : V.M.Guruprasath
-/* Date created : December 16, 2008
 /******************************************************************/
 
 char* cg_ltrim(char *string, char junk)
